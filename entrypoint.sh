8dd2ac90732c8c19fc85c945babe14a1387d4933
jnrdrgz <jnigrodriguez@gmail.com>
2022-01-16 18:35:21 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,8 +43,8 @@ EOF
 # - Build react bundle
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
-sh -c "npm ci" \
-&& sh -c "npm run build" \
+#sh -c "npm ci" \
+#&& sh -c "npm run build" \
 && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile react-deploy-to-s3-action \
               --no-progress \
